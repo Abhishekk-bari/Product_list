419f79a9a64632b376301080f0f7a1efb96b39c7
Abhi <abhi2bari929@gmail.com>
2025-01-08 16:13:24 +0530

    clear deploy error

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,12 +1,11 @@
 export const Banner = () => {
-    return (
-      <div className="h-5 text-center bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF,#FCD6FF)]">
-        <div className="container ">
-          <p className="font-medium">
-            <span className="sm:inline hidden"></span>
-          </p>
-        </div>
-  
+  return (
+    <div className="h-5 text-center bg-[linear-gradient(to_right,#FCD6FF,#29D8FF,#FFFD80,#F89ABF,#FCD6FF)]">
+      <div className="container ">
+        <p className="font-medium">
+          <span className="sm:inline hidden"></span>
+        </p>
       </div>
-    )
-  };
+    </div>
+  );
+};
